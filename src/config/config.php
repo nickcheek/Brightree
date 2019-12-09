acb6d44326379ba639e1da6880fb871d4d3970ea
Nick <nick@nicholascheek.com>
2019-12-09 07:27:44 -0600

    updated API definitions

--- src/config/config.php
+++ src/config/config.php
@@ -2,13 +2,13 @@
 
 return (object) [
 	'service'=>[
-        'document'=>'https://webservices.brightree.net/v0100-1610/DocumentationService/DocumentManagementService.svc',
+        'document'=>'https://webservices.brightree.net/v0100-1910/DocumentationService/DocumentManagementService.svc',
         'patient' => 'https://webservices.brightree.net/v0100-1802/OrderEntryService/patientservice.svc',
-        'documentation' => 'https://webservices.brightree.net/v0100-1811/DocumentationService/DocumentationService.svc',
+        'documentation' => 'https://webservices.brightree.net/v0100-1903/DocumentationService/DocumentationService.svc',
         'custom' => 'https://webservices.brightree.net/v0100-1610/CustomFieldService/CustomFieldService.svc',
         'insurance' => 'https://webservices.brightree.net/v0100-1610/OrderEntryService/InsuranceService.svc',
         'reference' => 'https://webservices.brightree.net/v0100-1904/ReferenceDataService/ReferenceDataService.svc',
-        'doctor' => 'https://webservices.brightree.net/v0100-1706/DoctorService/DoctorService.svc',
+        'doctor' => 'https://webservices.brightree.net/v0100-1904/DoctorService/DoctorService.svc',
         'inventory' => 'https://webservices.brightree.net/v0100-1709/InventoryService/InventoryService.svc',
         'pickup' => 'https://webservices.brightree.net/v0100-1610/OrderEntryService/PickupExchangeService.svc',
         'salesorder' => 'https://webservices.brightree.net/v0100-1908/OrderEntryService/SalesOrderService.svc',
@@ -16,5 +16,4 @@ return (object) [
         'security' => 'https://webservices.brightree.net/v0100-1906/SecurityService/UserSecurityService.svc',
         'pricing' => 'https://webservices.brightree.net/v0100-1908/InventoryService/PricingService.svc'
 	]
-	
 ];
