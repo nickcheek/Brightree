3f45a48204712fcb19abe826bed01ec8a2e6a39b
Nick <nick@nicholascheek.com>
2019-12-09 07:45:13 -0600

    changed example class

--- examples/example.myClass.php
+++ examples/example.myClass.php
@@ -3,6 +3,7 @@
 include 'vendor/autoload.php';
 
 use Nickcheek\Brightree\Brightree;
+use Carbon;
 
 class myClass {
 
@@ -13,15 +14,29 @@ class myClass {
         $username = 'nick@nicholascheek';
         $password = 'myPa$$word';
 
-        $this->bt = new Nickcheek\Brightree\Brightree($username,$password);
+        $this->bt = new Brightree($username,$password);
     }
 
-    public function getPatient($patient)
+    public function getSalesNum()
     {
-        return $this->bt->Patient()->PatientFetchByPatientID($patient);
+        $date = Carbon::now();
+        $today = $date->format('Y-m-d');
+        $order = [
+            'SearchParams' => [
+                'Branch' => ['ID' => 102],
+                'CreateDateTimeStart' => $today .'T00:00:00.000',
+                'CreateDateTimeEnd' => $today .'T23:59:59.000',
+            ],
+            'SortParams' => [],
+            'pageSize' => 10,
+            'page' => 1,
+        ];
+
+        $number = $this->bt->SalesOrder()->SalesOrderSearch($order);
+        return $number->SalesOrderSearchResult->TotalItemCount;
     }
 }
 
 $brightree = new myClass();
-$response = $brightree->getPatient(12345);
+$response = $brightree->getSalesNum();
 print_r($response);
