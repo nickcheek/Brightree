7255720278dc44bc5526aec12bd4e1fae3f520d6
Nicholas Cheek <nick@Nicholass-MBP.attlocal.net>
2020-07-23 10:56:36 -0500

    updated API definitions

--- src/Config/config.php
+++ src/Config/config.php
@@ -3,15 +3,15 @@
 return (object) [
 	'service'=>[
         'document'=>'https://webservices.brightree.net/v0100-1910/DocumentationService/DocumentManagementService.svc',
-        'patient' => 'https://webservices.brightree.net/v0100-1802/OrderEntryService/patientservice.svc',
-        'documentation' => 'https://webservices.brightree.net/v0100-1903/DocumentationService/DocumentationService.svc',
+        'patient' => 'https://webservices.brightree.net/v0100-2006/OrderEntryService/patientservice.svc',
+        'documentation' => 'https://webservices.brightree.net/v0100-2001/DocumentationService/DocumentationService.svc',
         'custom' => 'https://webservices.brightree.net/v0100-1610/CustomFieldService/CustomFieldService.svc',
-        'insurance' => 'https://webservices.brightree.net/v0100-1610/OrderEntryService/InsuranceService.svc',
+        'insurance' => 'https://webservices.brightree.net/v0100-2006/OrderEntryService/InsuranceService.svc',
         'reference' => 'https://webservices.brightree.net/v0100-1904/ReferenceDataService/ReferenceDataService.svc',
         'doctor' => 'https://webservices.brightree.net/v0100-1904/DoctorService/DoctorService.svc',
-        'inventory' => 'https://webservices.brightree.net/v0100-1709/InventoryService/InventoryService.svc',
-        'pickup' => 'https://webservices.brightree.net/v0100-1610/OrderEntryService/PickupExchangeService.svc',
-        'salesorder' => 'https://webservices.brightree.net/v0100-1908/OrderEntryService/SalesOrderService.svc',
+        'inventory' => 'https://webservices.brightree.net/v0100-2006/InventoryService/InventoryService.svc',
+        'pickup' => 'https://webservices.brightree.net/v0100-2007/OrderEntryService/PickupExchangeService.svc',
+        'salesorder' => 'https://webservices.brightree.net/v0100-2005/OrderEntryService/SalesOrderService.svc',
         'invoice' => 'https://webservices.brightree.net/v0100-1909/InvoiceService/InvoiceService.svc',
         'security' => 'https://webservices.brightree.net/v0100-1906/SecurityService/UserSecurityService.svc',
         'pricing' => 'https://webservices.brightree.net/v0100-1908/InventoryService/PricingService.svc'
