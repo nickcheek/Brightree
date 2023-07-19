f516f94ef5e11daeac1d54c8d4b17f2d627f12c6
Nicholas Cheek <nicholascheek@Nicholass-MBP-2.attlocal.net>
2023-07-19 16:09:11 -0400

    changed WSDL and added method for sales order

--- src/Config/config.php
+++ src/Config/config.php
@@ -8,10 +8,10 @@ return (object) [
         'custom' => 'https://webservices.brightree.net/v0100-1610/CustomFieldService/CustomFieldService.svc',
         'insurance' => 'https://webservices.brightree.net/v0100-2211/OrderEntryService/InsuranceService.svc',
         'reference' => 'https://webservices.brightree.net/v0100-1904/ReferenceDataService/ReferenceDataService.svc',
-        'doctor' => 'https://webservices.brightree.net/v0100-2008/DoctorService/DoctorService.svc',
+        'doctor' => 'https://webservices.brightree.net/v0100-2306/DoctorService/DoctorService.svc',
         'inventory' => 'https://webservices.brightree.net/v0100-2006/InventoryService/InventoryService.svc',
         'pickup' => 'https://webservices.brightree.net/v0100-2007/OrderEntryService/PickupExchangeService.svc',
-        'salesorder' => 'https://webservices.brightree.net/v0100-2203/OrderEntryService/SalesOrderService.svc',
+        'salesorder' => 'https://webservices.brightree.net/v0100-2306/OrderEntryService/SalesOrderService.svc',
         'invoice' => 'https://webservices.brightree.net/v0100-1909/InvoiceService/InvoiceService.svc',
         'security' => 'https://webservices.brightree.net/v0100-2204/SecurityService/UserSecurityService.svc',
         'pricing' => 'https://webservices.brightree.net/v0100-1908/InventoryService/PricingService.svc'
